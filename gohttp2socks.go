c9c6de897fce33274ccb986b30ac78a1b5f6a814
Tristan Hill <tristan@me.uk.eu.org>
2020-03-27 18:20:42 +0000

    Initial commit

---
+++ gohttp2socks.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "github.com/elazarl/goproxy"
+    "golang.org/x/net/proxy"
+    // "gopkg.in/elazarl/goproxy.v1"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    server := goproxy.NewProxyHttpServer()
+    server.Verbose = true
+    const proxyUrl = "socks5://127.0.0.1:1080"
+    os.Setenv("ALL_PROXY", proxyUrl)
+    server.ConnectDial = proxy.FromEnvironment().Dial
+    log.Println("Starting listening as http proxy on port 8080 forwarding to socks port 1080")
+    os.Setenv("HTTP_PROXY", proxyUrl)
+    log.Fatal(http.ListenAndServe("[::1]:8080", server))
+}
